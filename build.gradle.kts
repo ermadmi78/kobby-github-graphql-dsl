de7961d54b825c1c98318104a5e0c0823fa2dfc9
dermakov <ermadmi78@gmail.com>
2023-06-21 12:22:38 +0300

    Kobby release 3.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ buildscript {
 plugins {
     kotlin("jvm") version "1.8.21"
     kotlin("plugin.serialization") version "1.8.21"
-    id("io.github.ermadmi78.kobby") version "3.0.2"
+    id("io.github.ermadmi78.kobby") version "3.1.0"
 }
 
 kobby {

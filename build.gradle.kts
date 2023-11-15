d87142ba9e06d99ca01ef24c65924835bfb93cf8
dermakov <ermadmi78@gmail.com>
2023-11-15 20:12:10 +0300

    Upgrade versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,8 +13,8 @@ buildscript {
 }
 
 plugins {
-    kotlin("jvm") version "1.8.21"
-    kotlin("plugin.serialization") version "1.8.21"
+    kotlin("jvm") version "1.9.20"
+    kotlin("plugin.serialization") version "1.9.20"
     id("io.github.ermadmi78.kobby") version "3.1.0"
 }
 
@@ -36,16 +36,16 @@ kobby {
 }
 
 kotlin {
-    jvmToolchain(11)
+    jvmToolchain(17)
 }
 
 dependencies {
     // Add this dependency to enable Kotlinx Serialization
-    compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+    compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 
     // Add this dependency to use datetime scalars with Kotlinx Serialization support
-    compileOnly("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+    compileOnly("org.jetbrains.kotlinx:kotlinx-datetime:0.4.1")
 
     // Add this dependency to enable default Ktor adapters generation
-    compileOnly("io.ktor:ktor-client-cio:2.3.0")
+    compileOnly("io.ktor:ktor-client-cio:2.3.6")
 }
